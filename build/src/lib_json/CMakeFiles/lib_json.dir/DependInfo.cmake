3c98a9cf2a9664a8a19fe024e0d35281fe753534
AutoCoder <tj_liyuan@163.com>
2015-11-06 07:56:48 +0800

    porting to linux 2)

---
+++ build/src/lib_json/CMakeFiles/lib_json.dir/DependInfo.cmake
@@ -0,0 +1,20 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/liyuan/Opensource/AutoTrader/src/lib_json/json_reader.cpp" "/home/liyuan/Opensource/AutoTrader/build/src/lib_json/CMakeFiles/lib_json.dir/json_reader.cpp.o"
+  "/home/liyuan/Opensource/AutoTrader/src/lib_json/json_value.cpp" "/home/liyuan/Opensource/AutoTrader/build/src/lib_json/CMakeFiles/lib_json.dir/json_value.cpp.o"
+  "/home/liyuan/Opensource/AutoTrader/src/lib_json/json_writer.cpp" "/home/liyuan/Opensource/AutoTrader/build/src/lib_json/CMakeFiles/lib_json.dir/json_writer.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../src/lib_json/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
---
+++ build/src/lib_json/CMakeFiles/lib_json.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "../../../bin/liblib_json.a"
+)
