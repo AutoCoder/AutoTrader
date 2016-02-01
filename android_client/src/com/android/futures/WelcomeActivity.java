d69e5eaa3701438eb93bebe8cebc2cb0c2608513
AutoCoder <tj_liyuan@163.com>
2016-02-01 18:07:33 +0800

    add login page for android client

--- android_client/src/com/android/futures/WelcomeActivity.java
+++ android_client/src/com/android/futures/WelcomeActivity.java
@@ -5,30 +5,46 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.widget.Button;
+import android.view.View;
+import android.widget.EditText; 
 
 public class WelcomeActivity extends Activity implements Handler.Callback {
 
 	private Handler handler;
 	private final int WHAT = 1987;
 	private final long DELAY_TIME = 1500;
+	private Button loginBtn;
+	private Button cancelBtn;
+	private EditText accountEdit;
+	private EditText pwdEdit;
+	private EditText brokerIdEdit;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_welcome);
 		handler = new Handler(this);
+		loginBtn = (Button) this.findViewById(R.id.login_in);
+		cancelBtn = (Button) this.findViewById(R.id.cancel);
+		loginBtn.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				//todo: send login info
+			}
+		});
 	}
 
 	@Override
 	protected void onResume() {
 		super.onResume();
-		new Thread(new Runnable() {
-
-			public void run() {
-				handler.sendEmptyMessageDelayed(WHAT, DELAY_TIME);
-
-			}
-		}).start();
+//		new Thread(new Runnable() {
+//
+//			public void run() {
+//				handler.sendEmptyMessageDelayed(WHAT, DELAY_TIME);
+//
+//			}
+//		}).start();
 	}
 
 	@Override
