786eecc0b74107bf7c69c61ccd626fa35c6308bb
AutoCoder <tj_liyuan@163.com>
2016-04-13 16:42:13 +0800

    add log

--- src/MyStrategy/MACrossStratgy.cpp
+++ src/MyStrategy/MACrossStratgy.cpp
@@ -106,7 +106,7 @@ bool MACrossStratgy::tryInvoke(const std::vector<TickWrapper>& data, TickWrapper
 	}
 
 	info.m_techvec = curPtr;
-	SYNC_DEBUG_LOG << "ShortMA:" <<curPtr->ShortMA() << "LongMA:" << curPtr->LongMA() << "Direction:" << (curPtr->IsTriggerPoint() ? "Up" : "Down") ;
+	SYNC_DEBUG_LOG << "ShortMA:" <<curPtr->ShortMA() << "LongMA:" << curPtr->LongMA() << "Direction:" << (curPtr->IsTriggerPoint() ? "Up" : "Down") << "Triggerd:" << (orderSingal ? "Yes" : "No");
 	return orderSingal;
 }
 
