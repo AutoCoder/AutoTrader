94c54d93b76d9bf25b4c53b5ec97db073cff6c71
AutoCoder <tj_liyuan@163.com>
2016-04-20 16:37:24 +0800

    remove debug_log in MACross Strategy

--- src/MyStrategy/MACrossStratgy.cpp
+++ src/MyStrategy/MACrossStratgy.cpp
@@ -108,7 +108,7 @@ bool MACrossStratgy::tryInvoke(const std::vector<TickWrapper>& data, TickWrapper
 	}
 
 	info.m_techvec = curPtr;
-	SYNC_DEBUG_LOG << "ShortMA:" <<curPtr->ShortMA() << "LongMA:" << curPtr->LongMA() << "Direction:" << (curPtr->IsTriggerPoint() ? "Up" : "Down") << "Triggerd:" << (orderSingal ? "Yes" : "No");
+	//SYNC_DEBUG_LOG << "ShortMA:" <<curPtr->ShortMA() << "LongMA:" << curPtr->LongMA() << "Direction:" << (curPtr->IsTriggerPoint() ? "Up" : "Down") << "Triggerd:" << (orderSingal ? "Yes" : "No");
 	return orderSingal;
 }
 
