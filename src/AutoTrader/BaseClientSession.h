AutoCoder/AutoTrader

commit 193eb9a079e75285a14dbaa6f7618b83b1708f9a

add trade.txt log to check strategy

src/AutoTrader/BaseClientSession.h
@@ -52,8 +52,9 @@ public:
 protected:
 	bool ExecutePendingOrder();
 
-	//if Order insert success, update PositionInfoReady flag
 	void OnRtnOrder(CThostFtdcOrderField* pOrder);
+
+	void OnRtnTrade(CThostFtdcTradeField* pTrade);
 
 protected:
 	std::string										m_userId;

src/AutoTrader/AutoTrader.cpp
@@ -190,6 +190,7 @@ int main(int argc, const char* argv[]){
 	auto console = spdlog::stdout_logger_mt("console");
 	auto file_logger = spdlog::rotating_logger_mt("file_logger", "logs/logfile", 1048576 * 5, 3);
 	auto file_debug_logger = spdlog::rotating_logger_mt("file_debug_logger", "logs/debug", 1048576 * 5, 3);
+	auto file_trade_logger = spdlog::rotating_logger_mt("file_trade_logger", "logs/trade", 1048576 * 5, 3);
 	file_logger->info() << "----------------------------------";
 	file_logger->info() << "Lanuch AutoTrader...";
 	//Test

src/AutoTrader/BaseClientSession.cpp
@@ -128,6 +128,10 @@ void BaseClientSession::OnRtnOrder(CThostFtdcOrderField* pOrder){
 
 }
 
+void BaseClientSession::OnRtnTrade(CThostFtdcTradeField* pTrade){
+	SYNC_TRADE_LOG << CommonUtils::ConvertTradeToString(*pTrade);
+}
+
 void BaseClientSession::StopTrade(){
 	if (!m_isTrading.load())
 		return;

src/AutoTrader/ClientSession.cpp
@@ -145,6 +145,8 @@ void ClientSession::OnRtnOrder(CThostFtdcOrderField* pOrder){
 }
 
 void ClientSession::OnRtnTrade(CThostFtdcTradeField* pTrade){
+	BaseClientSession::OnRtnTrade(pTrade);
+
 	long long timeStamp = CommonUtils::DateTimeToTimestamp(pTrade->TradeDate, pTrade->TradeTime) * 2;
 	Transmission::Utils::SendDealInfo(m_session, Transmission::TRADE, pTrade->InstrumentID, pTrade->Direction, pTrade->OffsetFlag, pTrade->Price, pTrade->Volume, pTrade->OrderRef, timeStamp);
 }

src/Common/CommonUtils.cpp
@@ -302,6 +302,31 @@ std::string CommonUtils::ConvertOrderToString(const CThostFtdcOrderField& order)
 	return result.str();
 }
 
+std::string CommonUtils::ConvertTradeToString(const CThostFtdcTradeField& trade){
+	std::stringstream result;
+	
+	result << "合约代码:" << trade.InstrumentID << std::endl
+		<< " 用户代码:" << trade.UserID << std::endl
+		<< " 成交编号:" << trade.TradeID << std::endl
+		<< " 买卖方向:" << trade.Direction << std::endl
+		<< " 开平标志:" << trade.OffsetFlag << std::endl
+		<< " 投机套保标志:" << trade.HedgeFlag << std::endl
+		<< " 价格:" << trade.Price << std::endl
+		<< " 数量:" << trade.Volume << std::endl
+		<< " 成交时间:" << trade.TradeTime << std::endl
+		<< " 成交类型:" << trade.TradeType << std::endl
+		<< " 报单编号:" << trade.OrderSysID << std::endl
+		<< " 报单引用:" << trade.OrderRef << std::endl
+		<< " 本地报单编号:" << trade.OrderLocalID << std::endl
+		<< " 业务单元:" << trade.BusinessUnit << std::endl
+		<< " 序号:" << trade.SequenceNo << std::endl
+		<< " 经纪公司报单编号:" << trade.BrokerOrderSeq << std::endl
+		<< " 成交时期:" << trade.TradeDate << std::endl
+		<< " 交易日:" << trade.TradingDay << std::endl;
+
+	return result.str();
+}
+
 std::string CommonUtils::ConvertOrderListToString(const std::vector< CThostFtdcOrderField >& list){
 	if (list.empty())
 		return "";

src/Common/CommonUtils.h
@@ -38,7 +38,8 @@ public:
 	static bool InSameMinute(const std::string& time1, const std::string& time2);
 	static std::string StringFromStruct(const CThostFtdcInputOrderField& innerStruct);
 
-	static std::string ConvertOrderToString(const CThostFtdcOrderField& list);
+	static std::string ConvertOrderToString(const CThostFtdcOrderField& order);
+	static std::string ConvertTradeToString(const CThostFtdcTradeField& trade);
 	static std::string ConvertOrderListToString(const std::vector< CThostFtdcOrderField >& list);
 	static std::string ConvertTradeListToString(const std::vector< CThostFtdcTradeField >& list);
 

src/Common/crossplatform.h
@@ -28,6 +28,7 @@
 #define SYNC_PRINT spdlog::get("console")->info()
 #define SYNC_LOG spdlog::get("file_logger")->info()
 #define SYNC_DEBUG_LOG spdlog::get("file_debug_logger")->info()
+#define SYNC_TRADE_LOG spdlog::get("file_trade_logger")->info()
 #endif
 
 #endif

commit 71b947b21c1efb4c7ab1e6c4b80b806facd102fb

fix build break caused by 193eb9a079e75285a14dbaa6f7618b83b1708f9a

src/AutoTrader/BaseClientSession.h
@@ -17,6 +17,7 @@ class CtpTradeSpi;
 class RealTimeDataProcessor;
 class CThostFtdcTraderApi;
 struct CThostFtdcOrderField;
+struct CThostFtdcTradeField;
 
 namespace AP{
 	class AccountDetailMgr;